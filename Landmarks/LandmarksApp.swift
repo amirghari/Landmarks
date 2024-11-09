c1a3dc8189826c6310aad5b059e6ad42cba4c33a
Amir Ghari <amir.ghari@metropolia.fi>
2024-11-09 18:31:13 +0200

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Amir Ghari on 11/7/24.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
